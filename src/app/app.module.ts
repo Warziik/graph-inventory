77f21de7395aca132a31c2c5457de21593bc39a8
Alex Chesnay <me@alex-chesnay.fr>
2019-09-09 19:40:22 +0200

    feat: création de la vue de recherche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material-module';
+import { SearchComponent } from './search/search.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MaterialModule,
     AppRoutingModule
   ],
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  searchForm: FormGroup;
+
+  oses: Object[] = [
+    { id: 1, name: 'Windows' },
+    { id: 2, name: 'Linux' }
+  ]
+
+  architectures: Object[] = [
+    { id: 1, name: '32-bit' },
+    { id: 2, name: '64-bit' }
+  ]
+
+  versions: Object[] = [
+    { id: 1, name: '7' },
+    { id: 2, name: 'XP' },
+    { id: 3, name: '2000' },
+    { id: 4, name: '1607' }
+  ]
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Rechercher");
+  }
+
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      os: new FormControl(''),
+      architecture: new FormControl(''),
+      version: new FormControl('')
+    })
+  }
+
+  onSubmitForm() {
+    console.log(this.searchForm.value);
+  }
+}
